274ea035a38163e0283ae8080c1c431add4da1c1
Vladimir77715 <kazarcev_vladimir@icloud.com>
2020-06-02 20:54:14 +0300

    test

--- ClickerServicesModal.js
+++ ClickerServicesModal.js
@@ -874,6 +874,7 @@ var crel = createCommonjsModule(function (module, exports) {
             overflow: auto; /* Enable scroll if needed */
             background-color: rgb(0,0,0); /* Fallback color */
             background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+            -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
         }
         
         .clicker-modal *:focus {
